4155ab9b1012475d3ded7de8c524192c183fe62f
Willyam da Silva Carlos de Castro <willyam.castro@samaejs.com.br>
2019-11-28 15:43:00 -0300

    bat paginas impressas imp. HP via SNMP

---
+++ countPagePrintedHP_v1.bat
@@ -0,0 +1,71 @@
+@echo off
+
+REM 
+REM Data: 12/11/2019 (atualizado ***)
+REM
+REM Autor: 
+REM
+REM Obtém contagem de páginas impressas e modelo do dispositivo, exportando para um TXT
+REM 
+REM Requer instalação de Net-SNMP - http://www.net-snmp.org/
+REM
+
+REM arquivo atual: %%"<"%~f0%%
+
+REM http://batchscript.blogspot.com/2012/12/aprenda-o-for-de-uma-vez-por-todas.html
+
+
+
+
+
+set tmpFile = %tmp%\snmpwalk_return.txt
+
+
+call:getFileAddress
+
+call:outputFile
+	
+goto:eof
+
+
+
+REM ============= // ======== Functions ========= // ==================
+
+:getFileAddress
+
+	for /f "Tokens=1,2 delims=-" %%a in (ip_impressoras.txt) do (
+		@echo %%b	%%a
+
+		call:getDataSNMP %%a		
+	)	
+
+	goto:eof
+
+
+
+REM Obtém dados via SNMP
+:getDataSNMP
+
+	REM Modelo do equipamento
+	snmpwalk -Cc -v 1 -c public %1 1.3.6.1.2.1.25.3.2.1.3.1 >> walk.txt
+	
+	REM Qtd de páginas impressas
+	snmpwalk -Cc -v 1 -c public %1 1.3.6.1.2.1.43.10.2.1.4 >> walk.txt
+
+	goto:eof
+
+
+REM Cria arquivo de saída com dados úteis
+:outputFile
+
+	echo; echo "SAIDAS FINAIS: "
+
+	for /f "Tokens=3 delims=:"  %%b in (walk.txt) do (
+	
+		echo TOKEN:%%b
+	)
+	
+	goto:eof
+
+REM // Fazer a leitura do arquivo criado em getDataSNMP e filtrar o conteúdo útil > Exportar para outro arquivo de forma organizada;
+REM Usar o delimitador " : " 
